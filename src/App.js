b18fede9cf92d58813db5d039e9b10c37e1a6c19
Jen <medtrakdeveloper1@MedTrakDeveloper1s-MacBook-Pro.local>
2021-03-18 18:22:21 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+	  	Hello, this is the beginning of the burger
+      </div>
+    );
+  }
+}
+
+export default App;
