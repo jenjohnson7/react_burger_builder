f32de0993fd733300226adb436617feff45bee51
Jen <medtrakdeveloper1@MedTrakDeveloper1s-MacBook-Pro.local>
2021-04-01 23:19:16 -0400

    lecture 127-130 prop type validation and rendering burger ingredients + number of ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,1 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient.js';
+import classes from './Burger.css';
+
+const burger = (props) => {
+	let transformatedIngredients = Object.keys(props.ingredients)
+		.map(igKey => {
+			return [...Array(props.ingredients[igKey])].map((_, i) => {
+				return <BurgerIngredient key={igKey + i} type={igKey}/>;
+			});
+		})
+		.reduce((arr, el) => {
+			return arr.concat(el)
+		}, []);
+
+	console.log(transformatedIngredients);
+	if (transformatedIngredients.length === 0){
+		transformatedIngredients = <p>Please start adding ingredients.</p>
+	}
+
+	return (
+		<div className={classes.Burger}>
+			<BurgerIngredient type="bread-top"/>
+			{transformatedIngredients}
+			<BurgerIngredient type="bread-bottom"/>
+		</div>
+	);
+};
+
+export default burger;
